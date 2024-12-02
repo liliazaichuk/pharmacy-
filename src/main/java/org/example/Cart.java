d80da7cea00ab957ef87c4b437f78b2f804531e5
liliazaichuk <lilizaychuk2005@gmail.com>
2024-12-02 16:05:25 +0200

    set up essential methods

--- src/main/java/org/example/Cart.java
+++ src/main/java/org/example/Cart.java
@@ -1,4 +1,22 @@
 package org.example;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Cart {
+    private Map<String, Integer> cartItems = new HashMap<>();
+
+    public void addToCart(String medicine, int quantity) {
+        cartItems.put(medicine, cartItems.getOrDefault(medicine, 0) + quantity);
+    }
+
+    public void removeFromCart(String medicine) {
+        cartItems.remove(medicine);
+    }
+
+    public void clearCart() {
+        cartItems.clear();
+    }
+
+    public Map<String, Integer> getCartItems() {
+        return cartItems;
+    }
 }
