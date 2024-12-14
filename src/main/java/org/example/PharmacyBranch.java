4d6c42770058cf9d52a55d0a59575cde82204560
liliazaichuk <lilizaychuk2005@gmail.com>
2024-12-14 19:45:09 +0200

    integrate library "Lombok" and pattern Builder

--- src/main/java/org/example/PharmacyBranch.java
+++ src/main/java/org/example/PharmacyBranch.java
@@ -1,41 +1,20 @@
 package org.example;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.List;
 import java.util.Map;
 
+@Data
+@Builder
 public class PharmacyBranch {
     private String name;
     private double xCoordinate;
     private double yCoordinate;
     private Map<String, Integer> inventory;
 
-    public PharmacyBranch(String name, double xCoordinate, double yCoordinate, Map<String, Integer> inventory) {
-        this.name = name;
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
-        this.inventory = inventory;
-    }
-
     public boolean hasMedicine(String medicine, int quantity) {
         return inventory.getOrDefault(medicine, 0) >= quantity;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public double getXCoordinate() {
-
-        return xCoordinate;
-    }
-
-    public double getYCoordinate() {
-        return yCoordinate;
-    }
-
-    public Map<String, Integer> getInventory() {
-
-        return inventory;
     }
 }
